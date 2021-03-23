421a575585da953488777bb79a6526152466b276
Bakare Peace <peaceultimatum41@outlook.com>
2021-03-23 15:26:39 +0100

    One display with multiple Buttons -  Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+function Button(props){
+  return (
+    <button onClick={() => props.onClickFunction(props.increment)}>
+      +{props.increment}
+    </button>
+  );
+}
+
+function Display(props){
+  return (
+    <div>{props.message}</div>
+  );
+}
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const incrementCounter = (incrementValue) => setCounter(counter+incrementValue);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button onClickFunction={incrementCounter} increment={1} />
+      <Button onClickFunction={incrementCounter} increment={5} />
+      <Button onClickFunction={incrementCounter} increment={10} />
+      <Button onClickFunction={incrementCounter} increment={100} />
+      <Display message={counter}/>
     </div>
   );
 }
